52fff0082bc5f49f2c817c967c0c6c66e045c79b
Ahsan Masood <ahsanmasood795@gmail.com>
2024-01-08 16:48:43 +0500

    Use of Computed option in vue createApp to output dynamic values

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const app = Vue.createApp({
     data() {
         return {
+            counter: 0,
             myName: '',
         };
+    },
+    computed: {
+        // we will use it like data property not like method [fulname()]
+        fullname() {
+            console.log('Running Again...');
+            if (this.myName === '') {
+                return "";
+            }
+            return this.myName + ' ' + "Masood"
+        },
     },
     methods: {
         setMyName(event) {
@@ -12,5 +23,5 @@ const app = Vue.createApp({
             this.myName = '';
         }
     }
-});
+})
 app.mount('#events');
