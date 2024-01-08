702935487fa63626be08d8b118a4f054aa2975cd
Ahsan Masood <ahsanmasood795@gmail.com>
2024-01-08 12:46:58 +0500

    Interpolation and Data Binding

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const app = Vue.createApp({
-    // data: function () {}
     data() {
-        //and it will always return Object
         return {
-            courseGoal: 'Finish the course and learn Vue!',
+            courseGoal: 'Learn Vue!',
+            courseGoalA: 'Finish the course and learn Vue!',
+            courseGoalB: 'Finish the course and Master Vue!',
+            htmlCourseGoal: '<h1>Master Vue!</h1>',
+            vueLink: "https://vuejs.org/"
         };
     },
+    methods: {
+        outputGoal() {
+            const randomNumber = Math.random();
+            if(randomNumber < 0.5){
+                return this.courseGoalA;
+            } else {
+                return this.courseGoalB;
+            }
+        },
+        outputHtmlGoal() {
+            return this.htmlCourseGoal;
+        }
+    },
 });
-// #user-goal is an id in an html tag on which you want to connect vue with
 app.mount('#user-goal');
