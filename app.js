359a2f87c01250a69d4150fb4bf91443ceed6e1b
Ahsan Masood <ahsanmasood795@gmail.com>
2024-01-08 15:49:12 +0500

    Two way binding using v-model directive

--- app.js
+++ app.js
@@ -1,32 +1,16 @@
 const app = Vue.createApp({
     data() {
         return {
-            counter: 0,
-            name: '',
-            confirmedName: '',
+            myName: '',
         };
     },
     methods: {
-        add(num) {
-            this.counter = this.counter + num;
+        setMyName(event) {
+            this.myName = event.target.value;
         },
-        reduce() {
-            this.counter--;
-        },
-        setName(event, lastName = '') {
-            this.name = event.target.value + ' ' + lastName;
-        },
-        confirmedInput(){
-            this.confirmedName = this.name
-        },
-        submitFormWithoutModifier(event) {
-            // without event modifier we have to prevent default behaviour by ourselve
-            event.preventDefault();
-            alert('Submitted  Without Event Modifier!')
-        },
-        submitFormWithModifier() {
-            alert('Submitted With Event Modifier!')
-        },
+        resetInput() {
+            this.myName = '';
+        }
     }
 });
 app.mount('#events');
