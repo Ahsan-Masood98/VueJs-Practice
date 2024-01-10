572d4f7df7fef7af86c0e86032cbf6dff7e68e29
Ahsan Masood <ahsanmasood795@gmail.com>
2024-01-10 13:02:16 +0500

    rendering list using v-for (a for loop) and deep dive in it

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const app = Vue.createApp({
   data() {
-    return { 
+    return {
       goals: [],
-      enterdGoalValue:'',
-     };
+      enterdGoalValue: '',
+    };
   },
   methods: {
     addGoal() {
-     this.goals.push(this.enterdGoalValue);
+      this.goals.push(this.enterdGoalValue);
+    },
+    removeGoal(idx) {
+      // splice will remove the index and 2nd arg means how many elements to remove 
+      this.goals.splice(idx, 1)
     }
   }
 });
