917389b263205aad9d3bb214894c9c674f35de70
Ahsan Masood <ahsanmasood795@gmail.com>
2024-01-19 12:54:52 +0500

    created header in layout, basecard in ui and dynamiclist component to make basic project layout

--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
 import { createApp } from 'vue';
+import App from './components/App.vue'
+import BaseCart from './components/UI/BaseCart.vue'
 
-createApp({}).mount('#app');
+const app = createApp(App);
+app.component('base-cart', BaseCart);
+app.mount('#app');
+
