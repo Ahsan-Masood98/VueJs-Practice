dbe0decb69fb37eda5c440fc998f2beaa8f106a3
Ahsan-Masood98 <ahsanmasood795@gmail.com>
2024-01-29 19:03:01 +0500

    Updating data with watchers and passing params as porps instead of using

--- src/components/teams/TeamMembers.vue
+++ src/components/teams/TeamMembers.vue
@@ -9,6 +9,7 @@
         :role="member.role"
       ></user-item>
     </ul>
+    <router-link to="/teams/t2">Go to team 2</router-link>
   </section>
 </template>
 
@@ -17,6 +18,7 @@ import UserItem from "../users/UserItem.vue";
 
 export default {
   inject: ["users", "teams"],
+  props: ['teamId'],
   components: {
     UserItem,
   },
@@ -26,19 +28,28 @@ export default {
       members: [],
     };
   },
+  methods: {
+    loadTeamMemebers(teamId) {
+      const selectedTeam = this.teams.find((team) => team.id === teamId);
+      const members = selectedTeam.members;
+      const selectedMembers = [];
+      for (const member of members) {
+        const selectedUser = this.users.find((user) => user.id === member);
+        selectedMembers.push(selectedUser);
+      }
+      this.members = selectedMembers;
+      this.teamName = selectedTeam.name;
+    },
+  },
   created() {
     // this hook will be called when the component is created
     // this.$route.path  // /teams/t1
-    const teamId = this.$route.params.teamId;
-    const selectedTeam = this.teams.find((team) => team.id === teamId);
-    const members = selectedTeam.members;
-    const selectedMembers = [];
-    for (const member of members) {
-      const selectedUser = this.users.find((user) => user.id === member);
-      selectedMembers.push(selectedUser);
-    }
-    this.members = selectedMembers;
-    this.teamName = selectedTeam.name
+    this.loadTeamMemebers(this.teamId);
+  },
+  watch: {
+    teamId(newId) {
+      this.loadTeamMemebers(newId);
+    },
   },
 };
 </script>
