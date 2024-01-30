68d875e95e1d085fe1ad450b1e6fa9b5ef23f8d1
Ahsan-Masood98 <ahsanmasood795@gmail.com>
2024-01-30 15:01:40 +0500

    mutating the state asyncronously using actions to change the state in vuex

--- src/main.js
+++ src/main.js
@@ -16,16 +16,26 @@ const store = createStore({
             state.counter = state.counter + payload.value;
         }
     },
+    actions: {
+        increment(context) {
+            setTimeout(() => {
+                context.commit('increment');
+            }, 2000);
+        },
+        increase(context, payload) {
+            context.commit("increase", payload);
+        }
+    },
     getters: {
         finalCounter(state) {
             return state.counter * 2;
         },
         normalizedCounter(_, getters) {
             const finalCounter = getters.finalCounter;
-            if(finalCounter > 100) {
+            if (finalCounter > 100) {
                 return 100;
-            } 
-            if(finalCounter < 0) {
+            }
+            if (finalCounter < 0) {
                 return 0;
             }
             return finalCounter;
