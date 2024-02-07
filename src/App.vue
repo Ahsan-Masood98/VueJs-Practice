5ca31f6ea2d07f3b9a3c52c5ec9e15a32bf6da85
Ahsan-Masood98 <ahsanmasood795@gmail.com>
2024-02-07 16:24:22 +0500

    added transition to modal dialog

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,14 @@
   </div>
   <div class="container">
     <!-- transition must only contains one direct child not multiple -->
-    <transition>
+    <!-- sif you want to use totaly diferent class names than what vue provides like when you are using third-party animation library -->
+    <!-- <transition enter-from-class= "some-class" enter-active-class="some-class"> -->
+    <transition name="para">
       <p v-if="paraIsVisible">This is only sometimes visible ...</p>
     </transition>
-      <button @click="toggleParagraph">Animate</button>
+    <button @click="toggleParagraph">Animate</button>
   </div>
-  <base-modal @close="hideDialog" v-if="dialogIsVisible">
+  <base-modal @close="hideDialog" :open="dialogIsVisible">
     <p>This is a test dialog!</p>
     <button @click="hideDialog">Close it!</button>
   </base-modal>
@@ -92,35 +94,29 @@ button:active {
   animation: slide-scale 0.3s ease-out forwards;
 }
 
-/* .v-enter-from {
+.para-enter-from {
   opacity: 0;
-  transform:  translateY(-30px);
-} 
-.v-enter-to {
+  transform: translateY(-30px);
+}
+.para-enter-to {
   opacity: 1;
-  transform:  translateY(0);
-} */
-/* .v-enter-active {
+  transform: translateY(0);
+}
+.para-enter-active {
   transition: all 0.3s ease-out;
-} */
-.v-enter-active {
-  animation: slide-fade 0.3s ease-out;
 }
 
-.v-leave-active {
-  animation: slide-fade 0.3s ease-in;
-}
-/* .v-leave-active {
+.para-leave-active {
   transition: all 0.3s ease-in;
-} */
-/* .v-leave-from {
-  opacity: 1;
-  transform:  translateY(0);
 }
-.v-leave-to {
+.para-leave-from {
+  opacity: 1;
+  transform: translateY(0);
+}
+.para-leave-to {
   opacity: 0;
-  transform:  translateY(30px);
-} */
+  transform: translateY(30px);
+}
 
 @keyframes slide-scale {
   0% {
@@ -131,20 +127,6 @@ button:active {
   }
   100% {
     transform: translateX(-150px) scale(1);
-  }
-}
-@keyframes slide-fade {
-  0% {
-    opacity: 0;
-    transform: translateY(-30);
-  }
-  50% {
-    opacity: 0.5;
-    transform: translateY(-15px);
-  }
-  100% {
-    opacity: 1;
-    transform: translateY(0);
   }
 }
 </style>
